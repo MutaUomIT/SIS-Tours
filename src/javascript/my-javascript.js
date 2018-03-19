d616594c7b401164eb9c6b845fc3c7e00f2a7b68
Blue <kasunkavinda.bc@gmail.com>
2018-03-19 22:35:58 +0530

    Home final

--- src/javascript/my-javascript.js
+++ src/javascript/my-javascript.js
@@ -27,4 +27,39 @@ $(document).ready(function () {
   });
 });
 
+//testimonials
+
+$(document).ready(function () {
+  $('.testimonial').slick({
+    infinite: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    arrows: true,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+});
+
 
