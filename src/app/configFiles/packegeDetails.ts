e56a565e49a7f5a0601ca57641002fd4e5e4ddd3
Prasanna Lakmal <rgplakmal@gmail.com>
2018-03-21 01:37:27 +0530

    home page packge loop

--- src/app/configFiles/packegeDetails.ts
+++ src/app/configFiles/packegeDetails.ts
@@ -3,14 +3,90 @@ export class PackageData {
     {
       "id":1,
       "days":5,
-      "mainTopic":'Test1',
+      "mainTopic":'4 NIGHTS 5 DAYS PACKAGE',
       "subTopic":'Honeymoon on the beach',
       "locationCovered":["Bentota","Hikkaduwa","Mirissa","Colombo"],
       "ativitiesCovered":["Visit Kande Vihare","Visit to Turtle Farm","Visit to Gem Museum","Madu Ganga Tour","Whale and dolphin watching at Mirissa","Visit Coral Gardens","Night out in a Club","City tour and Shopping in Colombo"],
       "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale and dolphin watching"],
       "imagePath":'../assets/images/packageImages/1',
       "backgroundImage":"1.jpg"
+    },
+    {
+      "id":2,
+      "days":6,
+      "mainTopic":'5 NIGHTS 6 DAYS PACKAGE',
+      "subTopic":'Beach Package',
+      "locationCovered":["Bentota","Mirissa","Yala","Hikkaduwa","Colombo"],
+      "ativitiesCovered":["Visit Kande Vihare","Visit to Turtle Farm","Visit to Gem Museum","Madu Ganga Tour","Whale and dolphin watching at Mirissa","Yala Safari","Visit Coral Gardens","Visit Tsunami Museum","Night out in a Club","City tour and Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale and dolphin watching"],
+      "imagePath":'../assets/images/packageImages/2',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":3,
+      "days":6,
+      "mainTopic":'5 NIGHTS 6 DAYS PACKAGE',
+      "subTopic":'Kalpitiya, Wilpattu, Sigiriya, Kandy, Negambo',
+      "locationCovered":["Kalpitiya","Wilpattu","Sigiriya","Kandy","Negombo"],
+      "ativitiesCovered":["Explore Kalpitiya","Whale watching in Kalpitiya","Safari at Wilpattu","Anuradhapura Ruins","Dambulla Golden cave temple","Climb Sigiriya Rock","Hiriwadunna village tour","Spice gardens","Tooth relic temple","Cultural show","City tour in Kandy","Peradeniya Botanical Gardens","Pinnawala elephant orphanage","Night out in a Club","City tour and Shopping in Negombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale and dolphin watching"],
+      "imagePath":'../assets/images/packageImages/3',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":4,
+      "days":7,
+      "mainTopic":'6 NIGHTS 7 DAYS PACKAGE',
+      "subTopic":'Honeymoon Package - Kandy, Nuwara Eliya, Ella',
+      "locationCovered":["Kandy","Nuwaraeliya","Ella","Colombo"],
+      "ativitiesCovered":["Pinnawala Elephant orphanage","Cultural Show","Kandy Tooth Relic Temple","City tour and shopping in Kandy","Tea Factory Visit","Gregory’s Lake Visit","Victoria Park Visit","Climb Piduruthalagala","Visit Moon Plains","Spa treatment","Ravana Falls","Nine Arch Bridge","Night out in a Club","City tour and Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale watching"],
+      "imagePath":'../assets/images/packageImages/4',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":5,
+      "days":7,
+      "mainTopic":'6 NIGHTS 7 DAYS PACKAGE',
+      "subTopic":'Kandy, Nuwara Eliya, Yala, Mirissa, Colombo',
+      "locationCovered":["Kandy","Nuwaraeliya","Yala","Mirissa","Colombo"],
+      "ativitiesCovered":["Pinnawala Elephant orphanage","Kandy Tooth Relic Temple","City tour in Kandy","Tea Factory Visit","Visit Victoria Park","Visit Gregory’s Lake","Worlds End – Horton Plains and Baker’s falls","Spa treatment","Climb Piduruthalagala","Yala Safari","Whale watching and Dolphin watching","City tour in Colombo","Night out in a Club","Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale watching"],
+      "imagePath":'../assets/images/packageImages/5',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":6,
+      "days":9,
+      "mainTopic":'8 NIGHTS 9 DAYS PACKAGE',
+      "subTopic":'Kalpitiya, Sigiriya, Mahiyangana, Thissamaharama, Udawalawa, Colombo',
+      "locationCovered":["Kalpitiya","Sigiriya","Mahiyangana","Thissamaharama","Udawalawa","Colombo"],
+      "ativitiesCovered":["Explore Kalpitiya","Whale watching in Kalpitiya","Safari at Minneriya","Climb Sigiriya Rock","Hiriwadunna village tour","Visit Dambulla cave temple","Safari at Wasgamuwa","Visit Rathna Ella","Visit Sorabora Lake","Visit Maduru Oya","Visit Veddha Village","Safari at Yala","Udawalawa Safari","Night out in a Club","City tour and Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale watching"],
+      "imagePath":'../assets/images/packageImages/6',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":7,
+      "days":10,
+      "mainTopic":'9 NIGHTS 10 DAYS PACKAGE',
+      "subTopic":'Kandy, Habarana, Pasikudah through Polonnaruwa, Trinco, Anuradhapura, Negambo',
+      "locationCovered":["Kandy","Habarana","Sigiriya","Polonnaruwa","Pasikuda","Trinco","Anuradhapura","Negombo"],
+      "ativitiesCovered":["Pinnawala Elephant Orphanage","Visit Saradiyel Village","Peradeniya Botanical Gardens","Visit Tooth Relic Temple","Cultural Show","City tour and Shopping in Kandy","Spice gardens","Dambulla cave temple","Climb Sigiriya Rock","Safari at Minneriya","Hiriwadunna village tour","Ruins at Polonnaruwa","Pasikuda Beach","Marble Beach","Kinniya Bridge","Koneshwaram Temple","Kanniya Hot Spring Wells","Ruins at Anuradhapura","Ancient city visit Anuradhapura","Climb Mihinthale Rock","Night out in a Club","City tour and Shopping in Negombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps"],
+      "imagePath":'../assets/images/packageImages/7',
+      "backgroundImage":"1.jpg"
+    },
+    {
+      "id":8,
+      "days":11,
+      "mainTopic":'9 NIGHTS 10 DAYS PACKAGE',
+      "subTopic":'Negambo, Anuradhapura, Polonnaruwa, Sigiriya, Kandy, Nuwaraeliya, Ella, Yala, Colombo',
+      "locationCovered":["Negombo","Anuradhapura","Polonnaruwa","Sigiriya","Kandy","Nuwaraeliya","Ella","Yala","Colombo"],
+      "ativitiesCovered":["Negombo City Tour and Shopping","Ruins and temples Anuradhapura","Ruins at Polonnaruwa","Dambulla Cave Temple","Climb Sigiriya Rock","Hiriwadunna village tour","Visit Spice Gardens","Cultural Show","City tour and Shopping at Kandy","Kandy tooth relic temple","Peradeniya botanical gardens","Tea Factory visit","World’s end tour – Horton Plains and Baker’s falls","Visit Gregory’s lake","Visit Victoria Park","Climb Piduruthalagala","Visit Ambewel farm and Strawberry farm","Visit nine arch bridge","Visit Rawana Falls","Visit Little Adam’s peaks","Visit Ella Rock","Safari at Yala","Night out in a Club","City tour and Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps"],
+      "imagePath":'../assets/images/packageImages/8',
+      "backgroundImage":"1.jpg"
     }
   ];
 }
-
