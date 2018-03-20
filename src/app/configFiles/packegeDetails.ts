ddbeb142660ad6af288b2d70a308e0e53f07e533
Prasanna Lakmal <rgplakmal@gmail.com>
2018-03-20 19:13:29 +0530

    home page packge loop

--- src/app/configFiles/packegeDetails.ts
+++ src/app/configFiles/packegeDetails.ts
@@ -8,7 +8,7 @@ export class PackageData {
       "locationCovered":["Bentota","Hikkaduwa","Mirissa","Colombo"],
       "ativitiesCovered":["Visit Kande Vihare","Visit to Turtle Farm","Visit to Gem Museum","Madu Ganga Tour","Whale and dolphin watching at Mirissa","Visit Coral Gardens","Night out in a Club","City tour and Shopping in Colombo"],
       "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale and dolphin watching"],
-      "imagePath":'./assets/images/packageImages/1',
+      "imagePath":'../assets/images/packageImages/1',
       "backgroundImage":"1.jpg"
     }
   ];
