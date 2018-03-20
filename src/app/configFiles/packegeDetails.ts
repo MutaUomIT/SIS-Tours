9dc326d9d6f09ab765105febb8f38771e8ec4945
Prasanna Lakmal <rgplakmal@gmail.com>
2018-03-20 13:17:25 +0530

    testimonial slider

--- src/app/configFiles/packegeDetails.ts
+++ src/app/configFiles/packegeDetails.ts
@@ -2,66 +2,15 @@ export class PackageData {
   public static packageList= [
     {
       "id":1,
-      "Name":'Package1',
+      "days":5,
+      "mainTopic":'Test1',
+      "subTopic":'Honeymoon on the beach',
+      "locationCovered":["Bentota","Hikkaduwa","Mirissa","Colombo"],
+      "ativitiesCovered":["Visit Kande Vihare","Visit to Turtle Farm","Visit to Gem Museum","Madu Ganga Tour","Whale and dolphin watching at Mirissa","Visit Coral Gardens","Night out in a Club","City tour and Shopping in Colombo"],
+      "inclusions":["Return Air Ticket in Sri Lankan airlines","Airport Pick up and Drop off","Private luxury Vehicle (Air Conditioned)","Fuel and Insurance for the vehicle","Accommodation on Full board and half board basis in Star class luxurious hotels","Extra meals on the way from A class restaurant","Drinking water bottles for entire tour","Private English speaking Guide","Entrance fees","Charges for Safari Jeeps","Charges for Whale and dolphin watching"],
       "imagePath":'./assets/images/packageImages/1',
-      "backgroundImage":"main.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":2,
-      "Name":'Package2',
-      "imagePath":'./assets/images/packageImages/2',
-      "backgroundImage":"p2.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":3,
-      "Name":'Package3',
-      "imagePath":'./assets/images/packageImages/3',
-      "backgroundImage":"p3.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":4,
-      "Name":'Package4',
-      "imagePath":'./assets/images/packageImages/4',
-      "backgroundImage":"p4.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":5,
-      "Name":'Package5',
-      "imagePath":'./assets/images/packageImages/5',
-      "backgroundImage":"p5.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":6,
-      "Name":'Package6',
-      "imagePath":'./assets/images/packageImages/6',
-      "backgroundImage":"p6.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":7,
-      "Name":'Package7',
-      "imagePath":'./assets/images/packageImages/7',
-      "backgroundImage":"p7.jpg",
-      "description":"Descriptions"
-    },
-    {
-      "id":8,
-      "Name":'Package8',
-      "imagePath":'./assets/images/packageImages/8',
-      "backgroundImage":"p8.png",
-      "description":"Descriptions"
-    },
-    {
-      "id":9,
-      "Name":'Package9',
-      "imagePath":'./assets/images/packageImages/2',
-      "backgroundImage":"p2.jpg",
-      "description":"Descriptions"
+      "backgroundImage":"1.jpg"
     }
   ];
 }
+
