02f699e4d329e577f50803624ac21a58f950ac2a
Prasanna Lakmal <rgplakmal@gmail.com>
2018-03-23 09:38:55 +0530

    daily activities

---
+++ src/app/configFiles/DailyActivities.ts
@@ -0,0 +1,24 @@
+export class DailyActivities {
+  public static DailyActivitiesList = [
+    {
+      "id":'1',
+      "name":'Pick up from air port'
+    },
+    {
+      "id":'',
+      "name":'Pick up from air port'
+    },
+    {
+      "id":'1',
+      "name":'Pick up from air port'
+    },
+    {
+      "id":'1',
+      "name":'Pick up from air port'
+    },
+    {
+      "id":'1',
+      "name":'Pick up from air port'
+    },
+  ]
+}
