5acd502fb2f12a08a491ff515e5d645689ecd6d2
Tharindu Jayasinghe <tharindu199411@gmail.com>
2018-04-07 01:01:08 +0530

    form validations and added new component for form handling

---
+++ src/app/views/send-inquiry/send-inquiry.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CountryList } from "../../configFiles/countryList";
+import { NgForm } from '@angular/forms';
+import { MailSendingService } from '../../services/mail-sending.service';
+import { MsgPopupService } from '../../services/msg-popup.service';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-send-inquiry',
+  templateUrl: './send-inquiry.component.html',
+  styleUrls: ['./send-inquiry.component.css']
+})
+export class SendInquiryComponent implements OnInit {
+
+  @ViewChild('f') inquiryFormModal: NgForm
+
+  countryList: any = [];
+
+  dateForm: Date = new Date();
+  date: Date = new Date();
+  private _bsValue: Date;
+  minDate = new Date();
+  dateArrival: any;
+
+  constructor(private datePipe: DatePipe, private mailSender: MailSendingService, private msgPopup: MsgPopupService) { }
+
+  ngOnInit() {
+    this.countryList = CountryList.countryListWithPhoneCode;
+  }
+
+  onSubmit() {
+
+    if (this.inquiryFormModal.valid) {
+      var dataPipe = new DatePipe("en-US");
+
+      var inquiryInModal = {
+        duration: this.inquiryFormModal.value.duration,
+        arrivalDate: dataPipe.transform(this.inquiryFormModal.value.arrivalDate, 'dd-MM-yyyy'),
+        email: this.inquiryFormModal.value.email,
+        name: this.inquiryFormModal.value.name,
+        country: this.inquiryFormModal.value.country.name,
+        mobile: this.inquiryFormModal.value.country.code + ' ' + parseInt(this.inquiryFormModal.value.mobile),
+        message: this.inquiryFormModal.value.message,
+        children: this.inquiryFormModal.value.children,
+        adults: this.inquiryFormModal.value.adults
+      }
+      this.sendInquiryWithCustomizePackages(inquiryInModal);
+    } else {
+      this.msgPopup.broadcastMessagePopupEventEmitter({
+        type: 'error',
+        msg: "Please fill Valid Data"
+      });
+    }
+  }
+
+  setCountryCode(obj) {
+
+    if (obj === null) {
+      this.inquiryFormModal.form.patchValue({ code: '' });
+      this.inquiryFormModal.form.patchValue({ mobile: '' });
+    } else {
+      this.inquiryFormModal.form.patchValue({ code: obj.code })
+    }
+
+  }
+
+  private sendInquiryWithCustomizePackages(inquiryCustom) {
+    var mailObject = {
+      subject: '',
+      mailBody: ''
+    };
+
+    mailObject.subject = "Inquiry from Customize package";
+    mailObject.mailBody = '<p><bold>Tour Duration :</bold>' + inquiryCustom.duration + '</p><br/><p><bold>ArrivalDate :</bold>' + inquiryCustom.arrivalDate +
+      '</p><br/><p><bold>Country :</bold>' + inquiryCustom.country + '</p><br/><p><bold>Name :</bold>' + inquiryCustom.name + '</p><br/><p><bold>Mobile :</bold>'
+      + inquiryCustom.mobile +
+      '</p><br/><p><bold>Email :</bold><a href=' + inquiryCustom.email + '>' + inquiryCustom.email + '</a></p><br/><p><bold>No of adults :</bold>' + inquiryCustom.adults +
+      '</p></br><p><bold>No of children :</bold>' + inquiryCustom.children + '</p></br><p><bold>Message :</bold></p><br/>' + inquiryCustom.message;
+
+    this.mailSender.sendMail(mailObject).then((res: any) => {
+      if (res.status === 200) {
+        this.resetForm();
+      }
+    });
+  }
+
+  resetForm() {
+    this.inquiryFormModal.reset();
+    this.inquiryFormModal.form.patchValue({ duration: '' })
+  }
+
+
+}
