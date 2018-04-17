9bca1837669905fc26b533c7f00274ff5557c708
Prasanna Lakmal <rgplakmal@gmail.com>
2018-04-17 20:24:01 +0530

    add testimonials

--- src/app/configFiles/testimonials.ts
+++ src/app/configFiles/testimonials.ts
@@ -2,39 +2,66 @@ export class TestimonialData {
   public static testimonialList = [
     {
       "id": 1,
-      "name":'Christina Angel',
-      "country":'Australia',
-      "city":'Perth',
-      "feedback":'Everything was seamless. The private guides were interesting and knowledgeable. We felt that they were more than just guides.',
+      "name":'Mary Hale',
+      "country":'Englandng',
+      "city":'London',
+      "feedback":'Fantastic journey around Sri Lanka.  SIS tour operates gave me the best holiday experience in my life. I will recommend my friends and family to travel with SIS Tour operators.',
       "imagePath":'./assets/images/feedbackPic',
       "clientImage":"1.jpg"
     },
     {
       "id": 2,
-      "name":'Ryan Harris',
-      "country":'USA',
-      "city":'California',
-      "feedback":'It was like we had a friend in each city. I cannot speak highly enough about our 3 week trip to this place and the services provided by them.',
+      "name":'Waqas Hussain',
+      "country":'Saudi Arabia',
+      "city":'',
+      "feedback":'Best tour packages to experience the island Sri Lanka. Well planned tours. Will be back again. Friendly staff.',
       "imagePath":'./assets/images/feedbackPic',
       "clientImage":"2.jpg"
     },
     {
       "id": 3,
-      "name":'Amanda Jarvier',
-      "country":'England',
-      "city":'London',
-      "feedback":'I wanted to plan a unique trip to Sri Lanka. I had no idea where to start and then I found this. Perfect! will come again!',
+      "name":'S. Mahesh',
+      "country":'India',
+      "city":'',
+      "feedback":'Very friendly and helpful staff. Drivers are very safe and we had experienced so many beautiful places with the package we got with SIS tour operators.',
       "imagePath":'./assets/images/feedbackPic',
       "clientImage":"3.jpg"
     },
     {
       "id": 4,
-      "name":'Christina Angel',
-      "country":'Australia',
-      "city":'Perth',
-      "feedback":'Everything was seamless. The private guides were interesting and knowledgeable. We felt that they were more than just guides.',
+      "name":'Colin Johns',
+      "country":'USA',
+      "city":'',
+      "feedback":'We have selected the longest tour package. The staff at SIS tour operators didn’t let us feel any tiredness and make us comfortable throughout the tour. One of the best holidays we experienced. Thank you SIS.',
       "imagePath":'./assets/images/feedbackPic',
       "clientImage":"3.jpg"
     },
-    ];
+    {
+      "id": 5,
+      "name":'Mrs. A. Cooper',
+      "country":'',
+      "city":'',
+      "feedback":'We are honeymooners. SIS tour Operators made our holiday a excited one with many surprises. Friendly staff. Polite and helped us through. You will get a personal driver and a guide to make the tour comfortable.',
+      "imagePath":'./assets/images/feedbackPic',
+      "clientImage":"3.jpg"
+    },
+    {
+      "id": 6,
+      "name":'Albert  Schaffer',
+      "country":'German',
+      "city":'',
+      "feedback":'Amazing experience. The benefit of booking a tour through SIS tour Operators is you don’t need to worry about unnecessary payments. This is the 7th time we are in Sri Lanka. This time we enjoyed maximum and we paid extra only for our shopping. SIS Tour Operators looked after all the other expenses with our package. Happy to meet SIS Tour Operators. We will visit again.',
+      "imagePath":'./assets/images/feedbackPic',
+      "clientImage":"3.jpg"
+    },
+    {
+      "id": 7,
+      "name":'Jane Balmford',
+      "country":'England',
+      "city":'London',
+      "feedback":'Our Friend recommend us about SIS Tour Operators. They made travelling with our family easier. Comfortable vehicles. Friendly staff. Worth for money we pay. Very caring staff.',
+      "imagePath":'./assets/images/feedbackPic',
+      "clientImage":"3.jpg"
+    },
+  ];
 }
