a629c8d677d826b1e83f9c8e811987fd39c519be
Tharindu Jayasinghe <tharindu199411@gmail.com>
2018-04-20 21:00:41 +0530

    latest

--- src/app/configFiles/app.config.ts
+++ src/app/configFiles/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     public static mailServerConfiguration = {
-        IP_ADDRESS : 'localhost',
+        IP_ADDRESS : '35.177.223.83',
         PORT : '3000'
     }
         
